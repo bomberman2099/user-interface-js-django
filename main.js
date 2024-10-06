81f740e4ff51f2585bffcfc2710052f544d0d2f1
amirali <ososgol@gmail.com>
2024-10-06 19:45:36 +0330

    first commit| create base

---
+++ main.js
@@ -0,0 +1,11 @@
+function showCase(text){
+    document.querySelector('#content').innerHTML = text
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll("button").forEach((button)=>{
+        button.onclick = function () {
+            showCase(this.dataset.text)
+        }
+    })
+})
