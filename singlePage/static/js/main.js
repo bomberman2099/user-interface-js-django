95facff9f7b4e5e935c3cfe2e7fab0cbed6c4e63
amirali <ososgol@gmail.com>
2024-10-06 22:22:20 +0330

    Fix JS fetch to handle HTTP response and update DOM with Django data

--- singlePage/static/js/main.js
+++ singlePage/static/js/main.js
@@ -1,12 +1,14 @@
-function showCase(text){
-    
-    document.querySelector('#content').innerHTML = text
+function showCase(num){
+    fetch(`http://127.0.0.1:8000/section/${num}/`)
+    .then(Response => Response.text())
+    .then(text => document.querySelector('#content').innerHTML = text)
+    .catch(error => console.error('Error:', error));
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll("button").forEach((button)=>{
         button.onclick = function () {
-            showCase(this.dataset.text)
+            showCase(this.dataset.section)
         }
     })
 })
